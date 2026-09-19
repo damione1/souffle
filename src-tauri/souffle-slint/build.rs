4a9c96455e2b85dd27a3dc885f4d1d91d1d5cf4a
Damien Goehrig <14912510+damione1@users.noreply.github.com>
2026-09-19 18:59:52 -0400

    fix(ui): persist window geometry and increase default height (#276)

--- src-tauri/souffle-slint/build.rs
+++ src-tauri/souffle-slint/build.rs
@@ -26,6 +26,8 @@ fn main() {
     }
 
     let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
-    let config = slint_build::CompilerConfiguration::new().with_bundled_translations("lang").with_library_paths(library);
+    let config = slint_build::CompilerConfiguration::new()
+        .with_bundled_translations("lang")
+        .with_library_paths(library);
     slint_build::compile_with_config("ui/main_window.slint", config).expect("Slint build failed");
 }
